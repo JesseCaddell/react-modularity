12fb74552f63b3c9eb99e2d3d8b402fa5af81ad9
Midgar <80575182+JesseCaddell@users.noreply.github.com>
2024-03-02 16:15:52 -0800

    first commit

---
+++ src/components/ContentA.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContentA = () => {
+  return (
+    <div>
+      <h2>Content A</h2>
+      <p>This is Content A</p>
+    </div>
+  );
+}
+
+export default ContentA;
